dc69dbe7ef7396728e3a074bed08e4d635cfe2fd
Steven Almeroth <fedor@primesite.dev>
2023-11-08 23:19:49 -0500

    conda init

--- src/extra.bash
+++ src/extra.bash
@@ -66,3 +66,16 @@ source "$HOME/.cargo/env"
 
 # Ruby rbenv
 eval "$(rbenv init - bash)"
+
+# Python Conda initialize
+__conda_setup="$('/home/stav/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/stav/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/stav/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/stav/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
