54e389aee66b5c11264c91515277a327fadefff5
Steven Almeroth <steven.almeroth@gmail.com>
2025-10-05 17:07:43 -0400

    omarchy

---
+++ src/omarchy.bash
@@ -0,0 +1,5 @@
+# omarchy.bash: executed by profile.bash
+
+# All the default Omarchy aliases and functions
+# (don't mess with these directly, just overwrite them)
+source ~/.local/share/omarchy/default/bash/rc
