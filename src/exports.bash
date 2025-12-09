e954105654fb1c0b2a874617943acf4a925e5cf7
Steven Almeroth <steven.almeroth@gmail.com>
2025-12-09 11:05:17 -0500

    bun

--- src/exports.bash
+++ src/exports.bash
@@ -1,4 +1,7 @@
 # exports.bash: executed by profile.bash
 
-# Add `~/bin` to the `$PATH` : this is double dipping from ~/.profile
-# export PATH="$HOME/bin:$PATH";
+export PATH="$HOME/.local/bin:$PATH";
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
